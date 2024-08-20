1cb64ffb188d0c4177dae451abd383b660f22dff
Gaurav kumar <139734658+gauravkumar272003@users.noreply.github.com>
2024-08-20 13:52:44 +0530

    Create variables.js

---
+++ 01_test/variables.js
@@ -0,0 +1,20 @@
+const acc_id = 10112944
+let acc_email = "shdgk@gmaild.cin"
+
+//var works but dont use it as it will let the same variabel name to be used for other woork
+//creating issue
+var acc_pass = "1234552"
+
+//this is not the best way to declare variable in js
+acc_City = "Faridabad"
+
+//this will not work as acc_id is const variable
+// acc_id = 275
+
+console.log(acc_id);
+console.log(acc_email);
+console.log(acc_pass);
+console.log(acc_City);
+
+console.table([acc_email, acc_id, acc_pass, acc_City])
+
