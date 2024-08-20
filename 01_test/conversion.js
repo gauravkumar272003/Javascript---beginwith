2ed56c77278d64555c29bfc2db108ea8b5d20e86
Gaurav kumar <139734658+gauravkumar272003@users.noreply.github.com>
2024-08-20 11:06:09 +0000

    conversion of dataTypes

---
+++ 01_test/conversion.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+console.log(typeof score)
+
+let numbb = Number(score)
+
+console.log(typeof(numbb))
+
+
+console.log(numbb)
+//this will give NaN (not a number for 33a)
+
+let numb = 33;
+let conv = String(numb)
+
+console.log(typeof(conv))
+console.log(conv)
