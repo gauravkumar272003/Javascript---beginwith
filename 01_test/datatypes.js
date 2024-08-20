24e62c5d1daa7b5dd1b869163cdf9ad6956c1949
Gaurav kumar <139734658+gauravkumar272003@users.noreply.github.com>
2024-08-20 15:13:23 +0530

    Create datatypes.js

---
+++ 01_test/datatypes.js
@@ -0,0 +1,25 @@
+let nameof = "gaurav"
+let age = 69
+let ismad = true
+
+console.table([nameof, age, ismad])
+
+//data types in js
+/* 
+primitive data types 
+number
+bigIntegers
+strings
+boolean 
+null : it prints as object
+we use null to give back empty result 
+undefines : it prints as undefined
+symbol : unique
+
+objects 
+*/
+
+//it return the type of variable 
+console.log(typeof Boolean)
+console.log(typeof age)
+console.log(typeof null)
