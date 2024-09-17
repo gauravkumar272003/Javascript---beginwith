eb7b14e6e8c50a2848d7cb26791795b0a31d8403
Gaurav kumar <139734658+gauravkumar272003@users.noreply.github.com>
2024-09-17 18:53:22 +0000

    halo

---
+++ 01_test/convertionString.js
@@ -0,0 +1,23 @@
+let value = 3;
+let negval = -value;
+
+console.log(negval);
+
+let fname = "Gaurav "
+let lname = "Kumar"
+
+console.log(fname + lname);
+
+//start main 2 + 2 main addition ka operation perform hoga 
+console.log(2 + 2 + '2')
+
+//start se string jaise hin behave krega kyunki start bhi string se kia hai
+console.log('2' + 2 + 2)
+
+let gamecntr = 10;
+//post increment operator
+gamecntr++;
+
+console.log(gamecntr);
+//pre-increment operator
+console.log(++gamecntr);
